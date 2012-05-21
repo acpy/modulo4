f9292c8750cdc72b49504ccc8c644145adaebbf8
Luciano Ramalho <luciano@ramalho.org>
2012-05-20 21:49:18 -0300

    settings modular implementado; admin funcionando

--- circulante/circulante/settings.py
+++ circulante/circulante/settings.py
@@ -1,5 +1,10 @@
 # Django settings for circulante project.
 
+import os
+import glob
+
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -9,16 +14,7 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
+# DATABASES settings are in settings/??-databases*.py
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -27,11 +23,11 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/Sao_Paulo'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 SITE_ID = 1
 
@@ -81,7 +77,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = '9688(*5q-^m7&amp;#bk#vi^3!cd54g)uzr144#$7uwvfy*g#2goh='
+SECRET_KEY = 'c==3!ekj!(_k4r#8_#n5fq0mzn=rg$26&amp;@9=^dm8lyzn73*b#h'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -109,6 +105,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_PATH, 'templates'),
 )
 
 INSTALLED_APPS = (
@@ -119,7 +116,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
@@ -152,3 +149,10 @@ LOGGING = {
         },
     }
 }
+
+# this is based on the Transifex method of splitting settings into
+# several files within a settings/ directory, ordered by numeric prefixes;
+# here we use the .py extension to have syntax coloring of the files
+settings_files = glob.glob(os.path.join(PROJECT_PATH, 'settings', '*.py'))
+for f in sorted(settings_files):
+    execfile(os.path.abspath(f))
