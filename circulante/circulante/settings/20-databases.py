42effa7fd1a4078d0dbbac790d7a6609b6a77952
Luciano Ramalho <luciano@ramalho.org>
2012-05-22 17:17:56 -0300

    settings e instrucoes de insatalacao melhorados ao testar em outra maquina

--- circulante/circulante/settings/20-databases.py
+++ circulante/circulante/settings/20-databases.py
@@ -1,10 +1,10 @@
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(PROJECT_PATH, 'circulante.sqlite'), # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'circulante', # Or path to database file if using sqlite3.
+        'USER': 'circ_admin',    # Not used with sqlite3.
+        'PASSWORD': '????????',    # Not used with sqlite3.
+        'HOST': '127.0.0.1',              # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '5432',              # Set to empty string for default. Not used with sqlite3.
     }
 }
